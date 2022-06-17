756d42ac87c0160b375e951a108afdf1b4078980
SrPhantm <67681215+SrPhantm@users.noreply.github.com>
2022-06-17 04:45:45 -0500

    Uploaded Files

---
+++ main.py
@@ -0,0 +1,51 @@
+from os import listdir
+
+csv = 'enum,name,id\n'
+
+def convertLittleBytes(raw: int):
+    """Convert input to little bytes"""
+    raw = raw.to_bytes(4, 'little').hex()
+    processed = []
+    odd_byte = None
+    for byte in raw:
+        if odd_byte is None:
+            odd_byte = byte
+        else:
+            processed.append(odd_byte + byte)
+            odd_byte = None
+
+    return ' '.join(processed)
+
+for path in listdir('gamefiles/'):
+    if path.endswith('.cs') and '#' not in path:
+        with open('gamefiles/' + path, 'r') as f:
+            raw_data = f.readlines()
+        # Remove whitespace
+        raw_data = [s.strip() for s in raw_data]
+        # Remove namespace
+        raw_data = raw_data[2:][::-1][1:][::-1]
+
+        while True:
+            enum = raw_data.pop(0)[12:]
+            if enum == '':
+                break
+            print(enum)
+            
+            if raw_data.pop(0) != '{':
+                raise Exception('This file is not formatted correctly.')
+            
+            while raw_data[0] != '}':
+                entry = raw_data.pop(0)
+                entry = entry.strip(',')
+                entry = entry.split(' = ')
+                if not entry[0].startswith('///'):
+                    name, oid = entry[0], entry[1]
+                    oid = convertLittleBytes(int(oid))
+                    print(name, oid)
+                    csv += (f'{enum},{name},{oid}\n')
+
+        input('Press Enter...')
+
+with open('output.csv', 'w') as f:
+    f.truncate()
+    f.write(csv)
